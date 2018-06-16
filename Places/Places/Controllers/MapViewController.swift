82a5ad48bd28bef91cd092e0b270451c254c2fbe
Aluno <liviarsouza@gmail.com>
2018-06-16 09:29:22 -0300

    app completed

--- Places/Places/Controllers/MapViewController.swift
+++ Places/Places/Controllers/MapViewController.swift
@@ -11,29 +11,36 @@ import MapKit
 
 class MapViewController: UIViewController {
     
-    var places: [Place]!
-    
+    // MARK: Outlets
+    @IBOutlet weak var loading: UIActivityIndicatorView!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var viInfo: UIView!
     @IBOutlet weak var lbName: UILabel!
     @IBOutlet weak var lbAddress: UILabel!
     
+    // MARK: Variables
+    var places: [Place]!
+    var poi: [MKAnnotation] = []
+    lazy var locationManager = CLLocationManager()
+    var btUserLocation: MKUserTrackingButton!
+    var selectedAnnotation: PlaceAnnotation?
     
+    enum MapMessageType {
+        case routeError
+        case authorizationWarning
+    }
     
-    
-    
-    
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//         esconder os componentes
         searchBar.isHidden = true
         viInfo.isHidden = true
-
+        mapView.mapType = .mutedStandard
         mapView.delegate = self
+        locationManager.delegate = self
         
-        // configura o título
         if places.count == 1 {
             title = places[0].name
         } else {
@@ -43,58 +50,199 @@ class MapViewController: UIViewController {
         for place in places {
             addPlace(place)
         }
+        
+        configureLocationButton()
+        requestUserLocationAuthorization()
         showPlaces()
-//        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: Actions
+    @IBAction func showSearchBar(_ sender: UIBarButtonItem) {
+        self.searchBar.isHidden = !self.searchBar.isHidden
+    }
+    
+    @IBAction func showRoute(_ sender: UIButton) {
+        print("Show Route")
+        
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            showMessage(type: .authorizationWarning)
+            return
+        }
+        
+        if selectedAnnotation == nil {
+            showMessage(type: .routeError)
+        }
+        
+        let request = MKDirectionsRequest()
+        request.destination = MKMapItem(placemark:  MKPlacemark(coordinate: selectedAnnotation!.coordinate))
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: locationManager.location!.coordinate))
+        let directions = MKDirections(request: request)
+        directions.calculate { (directionResponse, error) in
+            if error == nil {
+                
+                if let response = directionResponse {
+                    self.mapView.removeOverlays(self.mapView.overlays)
+                    let route = response.routes.first!
+                    print("Nome: ", route.name)
+                    print("Distância: ", route.distance)
+                    print("Duração: ", route.expectedTravelTime)
+                    print("############################")
+                    
+                    for step in route.steps {
+                        print("Em \(step.distance) metro(s), \(step.instructions)")
+                    }
+                    
+                    // desenho da rota
+                    self.mapView.add(route.polyline, level: .aboveRoads)
+                    
+                    // mostrar toda a rota (gambiarra? - mas funciona)
+                    
+                    // fazer um filtro do que não é PlaceAnnotation (assim teremos somente a localizacao do usuario)
+                    var annotations = self.mapView.annotations.filter({!($0 is PlaceAnnotation)})
+                    // ja temos o atual PlaceAnnotation escolhida pelo usuario
+                    annotations.append(self.selectedAnnotation!)
+                    
+                    // mostrará o ponto que usuario está até o ponto de interesse escolhido
+                    self.mapView.showAnnotations(annotations, animated: true)
+                }
+                
+            } else {
+                self.showMessage(type: .routeError)
+            }
+        }
+    }
+    
+    
+    // MARK: MapView Functions
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKPolyline {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = UIColor(named: "main")?.withAlphaComponent(0.8)
+            renderer.lineWidth = 5.0
+            
+            return renderer
+        }
+        
+        return MKOverlayRenderer(overlay: overlay)
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("--->>> didSelect")
+        
+        // visualizando
+        let camera = MKMapCamera()
+        camera.centerCoordinate = view.annotation!.coordinate
+        camera.pitch = 80 // angulo da camera
+        camera.altitude = 100
+        mapView.setCamera(camera, animated: true)
+        
+        selectedAnnotation = (view.annotation as? PlaceAnnotation)
+        
+        //selectedAnnotation = (view.annotation as? PlaceAnnotation)
+        showInfo()
+    }
+    
+    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        viInfo.isHidden = true
+    }
+    
+    
+    // MARK: Functions
+    func showInfo() {
+        if selectedAnnotation != nil {
+            
+            lbName.text = selectedAnnotation?.title
+            lbAddress.text = selectedAnnotation?.address
+            viInfo.isHidden = false
+        }
     }
     
     func addPlace(_ place:Place) {
         print("Place -> \(place.name)")
         places.append(place)
         
-        // usando PlaceAnnotation do nosso model
         let annotation = PlaceAnnotation(coordinate: place.coordinate, type: .place)
         annotation.address = place.address
         annotation.coordinate = place.coordinate // variavel computada
         annotation.title = place.name
         mapView.addAnnotation(annotation)
-        
-//        // usando Annotations
-//        let annotation = MKPointAnnotation()
-//        annotation.coordinate = place.coordinate
-//        annotation.title = place.name
-//
-//        mapView.addAnnotation(annotation)
     }
     
     func showPlaces() {
         mapView.showAnnotations(mapView.annotations, animated: true)
     }
     
-    @IBAction func showSearchBar(_ sender: UIBarButtonItem) {
-        searchBar.isHidden = false
-        searchBar.becomeFirstResponder()
+    func configureLocationButton() {
+        btUserLocation = MKUserTrackingButton(mapView: mapView)
+        btUserLocation.backgroundColor = .white
+        btUserLocation.frame.origin.x = 10
+        btUserLocation.frame.origin.y = 10
+        btUserLocation.layer.cornerRadius = 5
+        btUserLocation.layer.borderWidth = 1
+        btUserLocation.layer.borderColor = UIColor(named: "main")?.cgColor
     }
     
-    @IBAction func showRoute(_ sender: UIButton) {
+    func requestUserLocationAuthorization() {
+        if CLLocationManager.locationServicesEnabled() {
+            // usario esta com esse recurso habilitado no momento
+            
+            switch CLLocationManager.authorizationStatus() {
+            case .authorizedWhenInUse, .authorizedAlways: // precisa ter os dois?
+                
+                // TODO
+//                print("mostrar botao de localizacao no mapa")
+                mapView.addSubview(btUserLocation)
+                
+            case .denied:
+                showMessage(type: .authorizationWarning)
+            case .notDetermined:
+                // request verification here
+                locationManager.requestWhenInUseAuthorization()
+            case .restricted:
+                // TODO
+                break
+            }
+        } else {
+            // precisa habilitar localizacao para acessar essa tela
+            showMessage(type: .authorizationWarning)
+        }
+    }
+    
+    func showMessage(type: MapMessageType) {
         
+        let title: String, message: String
+        var hasConfirmation: Bool = false
+        
+        switch type {
+        case .authorizationWarning:
+            title = "Requer permissão"
+            message = "Gostaria de acessar as configurações do app. para autorizar a permissão de 'Localização' ?"
+            hasConfirmation = true
+            
+        case .routeError:
+            title = "Erro"
+            message = "Error ao tentar traçar uma rota"
+        }
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        if hasConfirmation {
+            let confirmationAction = UIAlertAction(title: "Ajustes", style: .default) { (action) in
+                print("User confirmou action - OK!!!")
+                
+                if let appSettings = URL(string: UIApplicationOpenSettingsURLString) {
+                    UIApplication.shared.open(appSettings, options: [:], completionHandler: nil)
+                }
+                
+                self.dismiss(animated: true, completion: nil)
+            }
+            alert.addAction(confirmationAction)
+        }
+        
+        self.present(alert, animated: true, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 
@@ -122,7 +270,7 @@ extension MapViewController: MKMapViewDelegate {
         annotationView?.annotation = annotation
         annotationView?.canShowCallout = true // informacoes extras na forma de um balao
         // usando Point of Interess
-        annotationView?.markerTintColor = type == .place ? UIColor(named: "main") : UIColor(named: "poi")
+        annotationView?.markerTintColor = type == .place ? UIColor(named: "main_color") : UIColor(named: "poi")
         annotationView?.glyphImage = type == .place ?  #imageLiteral(resourceName: "placeGlyph") :  #imageLiteral(resourceName: "poiGlyph")
         annotationView?.displayPriority = type == .place ? .required : .defaultHigh
         
@@ -137,7 +285,73 @@ extension MapViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.isHidden = true
         searchBar.resignFirstResponder()
-        // 1
+        
+        // começa animação do loading
+        loading.startAnimating()
+        
+        // prepara uma busca em pontos de interesse do mapa
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBar.text
+        
+        // em qual regiao será realizado a pesquisa
+        request.region = mapView.region
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            if error == nil {
+                guard let response = response else {
+                    DispatchQueue.main.async {
+                        self.loading.stopAnimating()
+                    }
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.mapView.removeAnnotations(self.poi)
+                    self.poi.removeAll()
+                    for item in response.mapItems {
+                        let annotation = PlaceAnnotation(coordinate: item.placemark.coordinate, type: .poi)
+                        annotation.title = item.name
+                        annotation.subtitle = item.phoneNumber
+                        annotation.address = Place.getFormattedAddress(with: item.placemark)
+                        self.poi.append(annotation)
+                    }
+                    self.mapView.addAnnotations(self.poi)
+                    self.mapView.showAnnotations(self.poi, animated: true)
+                }
+                
+            } else {
+                print(error.debugDescription)
+            }
+            
+            DispatchQueue.main.async {
+                self.loading.stopAnimating()
+            }
+        }
+    }
+}
+
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("--->>> didChangeAuthorization (respondendo a uma mudanca de status) <<<---")
+        
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            mapView.addSubview(btUserLocation)
+            locationManager.startUpdatingLocation()
+        case .denied:
+            showMessage(type: .authorizationWarning)
+        default:
+            // TODO
+            break
+        }
+    }
+    
+    // notificado quando tiver mudanca de localizacao
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations.last!)
     }
     
 }
